8655cc66dd70ec98b8ec451188d98cef8f5a608f
CadeWeiskopf <cweiskopf@thisiscsg.com>
2023-08-20 20:22:28 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { useEffect, useRef } from "react";
+
+const render = (status: string) => {
+  switch (status) {
+    case Status.LOADING:
+      return <>"loading"</>;
+    case Status.FAILURE:
+      return <>"error"</>;
+    case Status.SUCCESS:
+      return <>"success"</>;
+    default:
+      return <>"unknown"</>;
+  }
+};
+
+const MyMapComponent = ({
+  center,
+  zoom,
+}: {
+  center: google.maps.LatLngLiteral;
+  zoom: number;
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) {
+      throw Error("missing ref");
+    }
+
+    const newMap = new window.google.maps.Map(ref.current, {
+      center,
+      zoom,
+    });
+
+    const newMarker = new google.maps.Marker();
+    newMarker.setPosition(center);
+    newMarker.setLabel("TEST LABEL");
+    newMarker.setMap(newMap);
+  });
+
+  return (
+    <div
+      style={{ width: "100%", height: "100%" }}
+      ref={ref}
+      id="map"
+    />
+  );
+};
 
 function App() {
+  if (!process.env.REACT_APP_API_KEY) {
+    throw Error("missisng api key");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header>test</header>
+      <main>
+        <Wrapper
+          apiKey={process.env.REACT_APP_API_KEY || ""}
+          render={render}
         >
-          Learn React
-        </a>
-      </header>
+          <MyMapComponent
+            center={{ lat: 37.521002, lng: -77.6502728 }}
+            zoom={15}
+          />
+        </Wrapper>
+      </main>
+      <footer>test</footer>
     </div>
   );
 }
